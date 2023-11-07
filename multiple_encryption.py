19315daedc5b257aa3585d414699061de22c16b7
Dem <147620640+DeusExMachina2@users.noreply.github.com>
2023-11-07 23:47:51 +0800

    Update multiple_encryption.py

--- multiple_encryption.py
+++ multiple_encryption.py
@@ -9,6 +9,22 @@ def double_decrypt(ciphertext, key1, key2):
 	return extended_function(extended_function(ciphertext, key2, True), key1, True)
 
 
+def middle_crack(plaintext, ciphertext):
+	# 中间相遇攻击
+	mid_texts = {}
+	find_keys = []
+	
+	for key in range(pow(2, 16)):
+		mid_texts[extended_function(plaintext, key)] = key
+	
+	for key in range(pow(2, 16)):
+		mid_text = extended_function(ciphertext, key, True)
+		if mid_text in mid_texts:
+			find_keys.append([mid_texts[mid_text], key])
+	
+	return find_keys
+
+
 if __name__ == '__main__':
 	# 第四关：多重加密
 	input_text = "Hello world!"
@@ -19,23 +35,12 @@ if __name__ == '__main__':
 	
 	ciphertext = double_encrypt(input_text, key1, key2)
 	print(ciphertext)
-	
+
 	text = double_decrypt(ciphertext, key1, key2)
 	print(text)
 
 	# 中间相遇攻击
-	mid_texts = {}
-	find_keys = []
-	
-	for key in range(pow(2, 16)):
-		mid_texts[extended_function(input_text, key)] = key
-
-	for key in range(pow(2, 16)):
-		mid_text = extended_function(ciphertext, key, True)
-		if mid_text in mid_texts:
-			find_keys.append([mid_texts[mid_text], key])
-
-	print(find_keys)
+	print(middle_crack(input_text, ciphertext))
 	
 	# 三重加密
 	key3 = 0x7F7F
